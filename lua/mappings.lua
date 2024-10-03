bd6901369fc951401debb701e3bcedb7129faa75
phob0s <phob0s@phob0s.pl>
2024-10-03 22:45:30 +0200

    update editor

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,47 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Surround
+map("x", "'", [[:s/\%V\(.*\)\%V/'\1'/ <CR>]], { desc = "Surround selection with '" })
+map("x", '"', [[:s/\%V\(.*\)\%V/"\1"/ <CR>]], { desc = 'Surround selection with "' })
+map("x", "*", [[:s/\%V\(.*\)\%V/*\1*/ <CR>]], { desc = "Surround selection with *" })
+
+map("n", "<leader>s*", [[:s/\<<C-r><C-w>\>/*<C-r><C-w>\*/ <CR>]], { desc = "Surround word with *" })
+map("n", '<leader>s"', [[:s/\<<C-r><C-w>\>/"<C-r><C-w>\"/ <CR>]], { desc = 'Surround word with "' })
+map("n", "<leader>s'", [[:s/\<<C-r><C-w>\>/'<C-r><C-w>\'/ <CR>]], { desc = "Surround word with '" })
+
+-- 
+
+-- LSP
+map("n", "<leader>lf", vim.diagnostic.open_float, { desc = "Diagnostics under cursor" })
+
+-- Telescope
+local telescope = require "telescope.builtin"
+
+map("n", "<leader>br", telescope.git_branches, { desc = "Telescope Git Branches" })
+map("n", "<leader>gs", telescope.git_status, { desc = "Telescope Git Status" })
+map("n", "<leader>gt", telescope.git_stash, { desc = "Telescope Git Stash" })
+
+map("n", "<leader>ca", require("actions-preview").code_actions, { desc = "Telescope LSP code action preview" })
+map("n", "<leader>fl", telescope.diagnostics, { desc = "Telescope find LSP diagnostics" })
+
+map("n", "<leader>fc", telescope.commands, { desc = "Telescope find commands" })
+map("n", "<leader>fr", telescope.registers, { desc = "Telescope find registers" })
+map("n", "<leader>fu", telescope.grep_string, { desc = "Telescope grep string under cursor" })
+map("n", "<leader>fb", telescope.current_buffer_fuzzy_find, { desc = "Telescope find current buffer" })
+
+map("n", "<leader>fk", telescope.keymaps, { desc = "Telescope find keymaps" })
+map("n", "<leader>fj", telescope.marks, { desc = "Telescope find marks" })
+map("n", "<leader>fp", telescope.filetypes, { desc = "Telescope find filetypes" })
+map("n", "<leader>fs", telescope.spell_suggest, { desc = "Telescope spell suggestion" })
+
+map("n", "<leader>ft", telescope.lsp_dynamic_workspace_symbols, { desc = "Telescope find LSP tags" })
+
+map("n", "<leader>fw", telescope.live_grep, { desc = "Telescope live grep" })
+map("n", "<leader>fz", function()
+  telescope.grep_string { search = "" }
+end, { desc = "Telescope fuzzy grep string" })
+
+-- Gitsigns
+map("n", "<leader>df", require("gitsigns").diffthis, { desc = "Git diff" })
+
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -9,17 +9,87 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      require("nvchad.configs.lspconfig").defaults()
       require "configs.lspconfig"
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+      opts = {
+        ensure_installed = {
+          "vim",
+          "lua",
+          "vimdoc",
+          "css",
+          "bash",
+          "c",
+          "css",
+          "diff",
+          "gitignore",
+          "html",
+          "ini",
+          "javascript",
+          "json",
+          "make",
+          "python",
+          "tmux",
+          "toml",
+          "xml",
+          "yaml",
+          "go",
+   		},
+    },
+  },
+
+  {
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {}
+    end,
+  },
+
+
+  {
+    "roobert/surround-ui.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "kylechui/nvim-surround",
+      "folke/which-key.nvim",
+    },
+    config = function()
+      require("surround-ui").setup {
+        root_key = "S",
+      }
+    end,
+  },
+
+  {
+    "aznhe21/actions-preview.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("actions-preview").setup {
+        highlight_command = {
+          require("actions-preview.highlight").delta(),
+        },
+        backend = { "telescope" },
+        telescope = {
+          sorting_strategy = "ascending",
+          layout_strategy = "vertical",
+          layout_config = {
+            width = 0.8,
+            height = 0.9,
+            prompt_position = "top",
+            preview_cutoff = 20,
+            preview_height = function(_, _, max_lines)
+              return max_lines - 15
+            end,
+          },
+        },
+      }
+    end,
+  },
+
 }
